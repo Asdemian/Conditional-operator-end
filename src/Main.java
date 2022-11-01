344d66254cc7eb34b66d5e290e3249d2077aeafd
hw commit <demaniks@yahoo.com>
2022-11-01 19:58:50 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        //task 1
+        int amanOfYears = 24;
+        if (amanOfYears >= 2 && amanOfYears <= 6) {
+            System.out.println("Если возраст человека равен " +amanOfYears+ " , то ему нужно ходить в детский садик.");
+        } else if (amanOfYears >= 7 && amanOfYears <= 18) {
+            System.out.println("Если возраст человека равен " +amanOfYears+ " , то ему нужно ходить в в школу.");
+        } else if (amanOfYears > 18 && amanOfYears < 24) {
+            System.out.println("Если возраст человека равен " +amanOfYears+ " , то ему нужно ходить в в школу.");
+        } else  if (amanOfYears >= 24) {
+            System.out.println("Если возраст человека равен " +amanOfYears+ " , то ему нужно ходить на работу.");
+        }
     }
 }
